be44c22460a869eba2db0b4ce4368c9eed6ca2f4
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2026-06-03 00:49:07 +0100

    Refactor and add README

---
+++ k8s/zeta-runner/src/main/java/com/schrodi/zeta_runner/config/K8sConfig.java
@@ -0,0 +1,38 @@
+package com.schrodi.zeta_runner.config;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.apis.AppsV1Api;
+import io.kubernetes.client.openapi.apis.AutoscalingV2Api;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class K8sConfig {
+    @Bean
+    public ApiClient apiClient() {
+        try {
+            ApiClient k8sClient = Config.defaultClient();
+            io.kubernetes.client.openapi.Configuration.setDefaultApiClient(k8sClient);
+            return k8sClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public CoreV1Api coreV1Api(ApiClient apiClient) {
+        return new CoreV1Api(apiClient);
+    }
+
+    @Bean
+    public AppsV1Api appsV1Api(ApiClient apiClient) {
+        return new AppsV1Api(apiClient);
+    }
+
+    @Bean
+    public AutoscalingV2Api autoscalingV2Api(ApiClient apiClient) {
+        return new AutoscalingV2Api(apiClient);
+    }
+}
