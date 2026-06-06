755708baea0516238e4d1c805d933e6ff10b80f0
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2026-06-06 02:50:39 +0100

    Add registry k8s manifests

---
+++ k8s/manifests/registry/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p ssl
+
+openssl genrsa -out ssl/ca.key 4096
+openssl req -new -x509 -days 1826 -key ssl/ca.key -out ssl/ca.crt -subj "/CN=zeta-registry-ca"
+
+openssl genrsa -out ssl/registry.key 4096
+openssl req -new -key ssl/registry.key -out ssl/registry.csr -subj "/CN=registry.zeta.svc.cluster.local"
+
+cat > ssl/registry-ext.cnf << EOF
+[SAN]
+subjectAltName=DNS:registry.zeta.svc.cluster.local,DNS:registry-svc,IP:127.0.0.1
+EOF
+
+openssl x509 -req -days 365 -in ssl/registry.csr \
+  -CA ssl/ca.crt -CAkey ssl/ca.key -CAcreateserial \
+  -out ssl/registry.crt -extfile ssl/registry-ext.cnf -extensions SAN
+
+# init registry
+export CA_CRT=$(base64 -w 0 ssl/ca.crt)
+export TLS_CRT=$(base64 -w 0 ssl/registry.crt)
+export TLS_KEY=$(base64 -w 0 ssl/registry.key)
+envsubst < registry-ca-secret.yaml | kubectl create -f -
+envsubst < registry-tls-secret.yaml | kubectl create -f -
+kubectl create -f registry-pvc.yaml
+kubectl create -f registry-deploy.yaml
+kubectl create -f registry-svc.yaml
